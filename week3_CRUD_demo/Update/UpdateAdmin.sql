c91df9d3370d7f932b436a55af69220276db1cb3
adityaevanrhm <adityaevanrhm@gmail.com>
2025-11-21 09:35:36 +0700

    Enhance comments in UpdateAdmin.sql for clarity

--- week3_CRUD_demo/Update/UpdateAdmin.sql
+++ week3_CRUD_demo/Update/UpdateAdmin.sql
@@ -1,14 +1,14 @@
-SELECT * -- READ
+SELECT * -- READ / CHECK
 FROM Admin
 WHERE AdminID = 1;
 
-UPDATE Admin -- UPDATE
+UPDATE Admin -- UPDATE ADMIN INFO
 SET 
     Name = 'John Updated',
     Email = 'john.updated@example.com',
     Phone = 987654321
 WHERE AdminID = 1;
 
-SELECT * -- READ AFTER UPDATE
+SELECT * -- READ / CHECK AFTER UPDATE
 FROM Admin
 WHERE AdminID = 1;
