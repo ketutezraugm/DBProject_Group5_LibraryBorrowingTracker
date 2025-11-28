defc241076c79b55765e4a9ea942f06288688aef
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2025-11-28 15:27:29 +0700

    Delete week4/integration directory

--- week4/integration/library_backend/category/create_category.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once __DIR__ . "/../config/db.php";
-$name = $_POST['name'] ?? '';
-$stmt = $conn->prepare("INSERT INTO Category (CategoryName) VALUES (?)");
-$stmt->bind_param("s",$name);
-if ($stmt->execute()) echo json_encode(["status"=>"success","id"=>$conn->insert_id]);
-else { http_response_code(500); echo json_encode(["status"=>"error","message"=>$stmt->error]); }
-$stmt->close();
